ecc51ead5757ade03592bf95306406abb10bfc18
habeeb <habeebq@virtualbox.com>
2015-10-09 11:54:05 +0100

    Autocommit

--- blog/pelicanconf.py
+++ blog/pelicanconf.py
@@ -44,3 +44,4 @@ MENUITEMS = (('projects', '/projects.html'),
 THEME = '/home/habeeb/workspace/pelican_themes/pelican-bootstrap3'
 USE_LESS = True
 BOOTSTRAP_THEME = 'slate'
+PYGMENTS_STYLE  = 'solarizeddark'
